d7707d89b31109bc7205329bb0f5fff182681a2d
htmlcssjvscrpt <131257637+htmlcssjvscrpt@users.noreply.github.com opai 5vjqSG5fLFh6ztL top E5TbFu5mDZjx6QW chekio 39xZQydmo>
2023-07-21 22:32:30 +0800

    Add rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,30 +1,90 @@
 
-function playGame () {
+let comp_score = 0;
+let player_score = 0;
+
+function playRound (playerSelection) {
+    // Clear results before every game
+    clearResults();
 
     // List of items
     const my_items = ['rock', 'paper', 'scissors'];
     
     // Get computer's selction
     const compSelection = my_items[Math.floor(Math.random() * my_items.length)];
-    console.log(`\nThe computer's object is ${compSelection}`); 
-
-    // Prompt user for object
-    const playerSelection = prompt("Select an object: ", '').toLowerCase();
-    console.log(`Your object is ${playerSelection}`);
+ 
+    // Display computer and player object
+    objects(compSelection, playerSelection);
 
     // Compare objects and declare winner
     if (compSelection === playerSelection) {
-        console.log("We have a draw!")
+        winner('Draw', compSelection, playerSelection);
     } else if ((compSelection === 'rock' && playerSelection === 'paper') || 
                (compSelection === 'scissors' && playerSelection === 'rock') ||
                (compSelection === 'paper' && playerSelection === 'scissors')) {
-        console.log(`You win! ${playerSelection} beats ${compSelection}!`)
+        winner('You', compSelection, playerSelection);
+        player_score += 1;
     } else {
-        console.log(`Computer wins! ${compSelection} beats ${playerSelection}!`)
+        winner('Computer', compSelection, playerSelection);
+        comp_score += 1;
     }
 }
 
-// Play five games
-for (let i = 0; i < 6; i++) {
-    playGame()
+// Display computer and player object
+function objects(compSelection, playerSelection) {
+
+    const container = document.querySelector('#results');
+    const para1 = document.createElement('p');
+    para1.textContent = `\nThe computer's object is ${compSelection}\n`;
+    container.append(para1);
+
+    const para2 = document.createElement('p');
+    para2.textContent = `Your object is ${playerSelection}`;
+    container.append(para2)
 }
+
+// Declare draw or winner
+function winner(who, compSelection, playerSelection) {
+    const container = document.querySelector('#results');
+    const para1 = document.createElement('p');
+    if (compSelection === playerSelection) {
+       para1.textContent = 'We have a draw!';
+       container.append(para1);
+    } else {
+        para1.textContent = `${who} win! ${playerSelection} beats ${compSelection}!`;
+        container.append(para1);
+    }
+
+    const para2 = document.createElement('p');
+    para2.textContent = `Computer score: ${comp_score}. player score ${player_score}`;
+    container.append(para2);
+
+    const para3 = document.createElement('p');
+    if (comp_score === 5) {
+        para3.textContent = 'COMPUTER is the ULTIMATE WINNER!!';
+        container.append(para3);
+    } else if (player_score === 5) {
+        para3.textContent = 'YOU are the ULTIMATE WINNER!!';
+        container.append(para3);
+    }
+}
+
+// Clear the results
+function clearResults() {
+    const container = document.querySelector('#results');
+    container.innerHTML='';
+}
+
+let rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', () => {
+    playRound('rock');
+});
+
+let paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', () => {
+    playRound('paper');
+});
+
+let buttonScissors = document.querySelector('#scissors');
+buttonScissors.addEventListener('click', () => {
+    playRound('scissors');
+});
