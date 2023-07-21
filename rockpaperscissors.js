ed7fefea26605169f5e690834767e3a5d31f805d
htmlcssjvscrpt <131257637+htmlcssjvscrpt@users.noreply.github.com opai 5vjqSG5fLFh6ztL top E5TbFu5mDZjx6QW chekio 39xZQydmo>
2023-07-21 17:53:48 +0800

    Add rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,66 +1,30 @@
 
-function game () {
+function playGame () {
 
-    // list of items
+    // List of items
     const my_items = ['rock', 'paper', 'scissors'];
-
-    // Computer selects random object and returns the object
-    function computerSelection() {
-        const selection = Math.floor(Math.random() * my_items.length);
-        return selection;
-    }
-
-    let compSelection = computerSelection();
-
-    // change compChoice from number to object
-    switch (compSelection) {
-        case 0:
-            compSelection = 'rock';
-            break;
-        case 1:
-            compSelection = 'paper';
-            break;
-        case 2:
-            compSelection = 'scissors'
-            break;
-        default:
-            console.log("you've chosen a strange object!");
-    }
-
-    console.log(compSelection); 
+    
+    // Get computer's selction
+    const compSelection = my_items[Math.floor(Math.random() * my_items.length)];
+    console.log(`\nThe computer's object is ${compSelection}`); 
 
     // Prompt user for object
-    let userObject;
-    let playerSelection = prompt("Select an object: ", userObject);
-    console.log(playerSelection);
+    const playerSelection = prompt("Select an object: ", '').toLowerCase();
+    console.log(`Your object is ${playerSelection}`);
 
-    // possible combinations
-    // r, r : p, p: s, s
-    // r, p / p, r
-    // r, s / s, r
-    // p, s / s, p
-
-    // computer user choice and computer choice and declare winner
-    if ((compSelection === 'rock' && playerSelection === 'rock') ||  (compSelection === 'paper' && playerSelection === 'paper') || (compSelection === 'scissors' && playerSelection === 'scissors')) {
-        console.log("We have a draw!");
-    } else if (compSelection === 'rock' && playerSelection === 'paper') {
-        console.log(`You wins! ${playerSelection} beats ${compSelection}`)
-    } else if (compSelection === 'paper' && playerSelection === 'rock') {
-        console.log(`Computer wins! ${compSelection} beats ${playerSelection}`)
-    } else if (compSelection === 'rock' && playerSelection === 'scissors') {
-        console.log(`Computer wins! ${compSelection} beats ${playerSelection}`)
-    } else if (compSelection === 'scissors' && playerSelection === 'rock') {
-        console.log(`You win! ${playerSelection} beats ${compSelection}`)
-    } else if (compSelection === 'paper' && playerSelection === 'scissors') {
-        console.log(`You win! ${playerSelection} beats ${compSelection}`)
-    } else if (compSelection === 'scissors' && playerSelection === 'paper') {
-        console.log(`Computer win!s ${compSelection} beats ${playerSelection}`)
+    // Compare objects and declare winner
+    if (compSelection === playerSelection) {
+        console.log("We have a draw!")
+    } else if ((compSelection === 'rock' && playerSelection === 'paper') || 
+               (compSelection === 'scissors' && playerSelection === 'rock') ||
+               (compSelection === 'paper' && playerSelection === 'scissors')) {
+        console.log(`You win! ${playerSelection} beats ${compSelection}!`)
+    } else {
+        console.log(`Computer wins! ${compSelection} beats ${playerSelection}!`)
     }
-
 }
 
+// Play five games
 for (let i = 0; i < 6; i++) {
-    game()
+    playGame()
 }
-
-
